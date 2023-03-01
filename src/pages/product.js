f4a447e06242b30adc060db7265f13388c39e0e4
CoderOfPHCity <Agantemboy@gmail.com>
2023-03-01 11:32:26 +0100

    updated

---
+++ src/pages/product.js
@@ -0,0 +1,14 @@
+
+// import React and Next components
+import React from 'react';
+
+// create the product page component
+const ProductPage = () => {
+  return (
+    <>
+    <h1>Hello dear, This is the product page.</h1>
+    </>
+  );
+};
+
+export default ProductPage;
